0753cb66b927d36fcf37c9de9658ef2429bacc12
oidolee <oidolee@gmail.com>
2024-02-06 22:31:34 +0900

    2024-02-06 push

--- src/main/webapp/resource/page_3/calenderJs/calender.js
+++ src/main/webapp/resource/page_3/calenderJs/calender.js
@@ -92,7 +92,8 @@ const renderCalendar = () => {
                                     ${games[selectMonth][dayIndex]['time']}
                                 </p>
                                 <div>
-                                	<button type="button" class="btn btn-outline-success">경기예약</button>
+                                	<button onclick="purchaseTicket(${viewYear}, ${viewMonth+1}, ${date}, '${games[selectMonth][dayIndex]['time']}')" type="button" class="btn btn-success">경기예약</button>
+
                                     <svg onclick="deleteGames(${games[selectMonth][dayIndex]['DG_No']})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -162,6 +163,18 @@ const deleteGames = (DG_No) => {
 
 //추가 
 const addGames = (year,month,day) =>{
-    console.log(year,month,day)
     location.href = "./gamesInsert.gc?year="+year+"&month="+month+"&day="+day;
 }
+
+//예약 기능 
+const purchaseTicket = (year, month, day, time) => {
+    const p_month = month < 10 ? "0" + month : month;
+    const p_day = day < 10 ? "0" + day : day;
+    const p_time = time; // 시간 변수 추가
+    
+    const purchaseDate = year + "-" + p_month + "-" + p_day + " " + p_time; // 변수명 수정
+    
+    // 예약 페이지로 이동할 때 구성된 날짜 정보를 전달합니다.
+    location.href = "./purchaseTicket.tc?purchaseDate=" + purchaseDate;
+}
+
